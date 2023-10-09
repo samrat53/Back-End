afeaa39efbd09e3044b5af4802c445097315c795
samrat53 <samrat05hccs@gmail.com>
2023-10-10 02:00:58 +0530

    configure basic http request

--- expressLearn/index.js
+++ expressLearn/index.js
@@ -1,5 +1,14 @@
 import express from "express";
-const app=express();
-const PORT=5174;
+const app = express();
+const PORT = 5173;
 
-app.listen(PORT,()=>console.log(`listening on port ${PORT}`));
+let name = "samrat";
+app.get("/", (req, res) => res.send("<h1>GOAT in the making!</h1>"));
+app.get(`/samrat`, (req, res) => res.send(`<h1>${name}</h1>`));
+app.get(`/contact`, (req, res) =>
+  res.send(`<h1>Contact me:</h1><hr><p>Phone: 00880808988</p><p>Found in NITA</p>`)
+);
+app.get("/about", (req, res) => {
+  res.send("<h1>About me:</h1><hr><p>Diurnal Photographer</p><p>Nocturnal Coder</p>");
+});
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
