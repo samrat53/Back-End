b51abcba5837ec92ac6373531b5c63bd0c905c13
samrat53 <samrat05hccs@gmail.com>
2023-10-10 00:19:20 +0530

    add readme/ setup express.js

---
+++ README.md
@@ -0,0 +1,3 @@
+dependencies:
+1. express
+2. modemon (npm nodemon index.js)
---
+++ expressLearn/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+const app=express();
+const PORT=5174;
+
+app.listen(PORT,()=>console.log(`listening on port ${PORT}`));
